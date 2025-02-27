0bfd734ac02556a2291b0f3e0c7ead9b5f743673
Mariano Tami <mariano13age@gmail.com>
2025-02-27 20:13:32 -0300

    deploy

--- pi_development/web/views.py
+++ pi_development/web/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'index.html')
-
 from .models import Servicio
 
 def home(request):
     servicios = Servicio.objects.all()
     return render(request, 'index.html', {'servicios': servicios})
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def policies(request):
+    return render(request, 'policies.html')
+def projects(request):
+    return render(request, 'projects.html')
